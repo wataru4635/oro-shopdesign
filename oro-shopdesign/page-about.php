fc8d8a3c6d9285d3a6174739026d2216547f940b
ix-code4635 <ixcode.4635@gmail.com>
2025-11-07 11:56:43 +0900

    全ページ実装完了

--- oro-shopdesign/page-about.php
+++ oro-shopdesign/page-about.php
@@ -1,17 +1,175 @@
 <?php
 /*
-Template Name: 私たちについて
+Template Name: 会社紹介
 */
 ?>
 <?php get_header(); ?>
 
 <main>
 
-  <section class="about">
-    <div class="about__inner">
+  <section class="about-mv">
+    <div class="about-mv__inner">
+      <hgroup class="about-mv__content">
+        <p class="about-mv__en-title">about</p>
+        <h1 class="about-mv__title">
+          <img src="<?php echo IMAGEPATH; ?>/common/about-title.webp" alt="会社紹介" width="167.561" height="27.567" class="about-mv__title-img">
+        </h1>
+      </hgroup>
+
+      <div class="about-mv__img-wrap">
+        <img src="<?php echo IMAGEPATH; ?>/about/about-mv.webp" alt="コンクリート壁にoro architectsのロゴがある写真" width="613" height="461" class="about-mv__img" />
+      </div>
+    </div>
+  </section>
+
+  <section class="about-info">
+    <div class="about-info__inner">
+
+      <dl class="about-info__list">
+        <div class="about-info__group js-fade-up">
+          <dt class="about-info__term">会社名</dt>
+          <dd class="about-info__desc">
+            oro株式会社／<br>oro株式会社 一級建築士事務所
+          </dd>
+        </div>
+
+        <div class="about-info__group js-fade-up">
+          <dt class="about-info__term">本社所在地</dt>
+          <dd class="about-info__desc">
+            静岡県浜松市浜名区於呂1475番地の1
+          </dd>
+        </div>
+
+        <div class="about-info__group js-fade-up">
+          <dt class="about-info__term">役員</dt>
+          <dd class="about-info__desc">
+            代表取締役　藤森 輔
+          </dd>
+        </div>
+
+        <div class="about-info__group js-fade-up">
+          <dt class="about-info__term">設立</dt>
+          <dd class="about-info__desc">
+            2019年1月
+          </dd>
+        </div>
+
+        <div class="about-info__group js-fade-up">
+          <dt class="about-info__term">事業内容</dt>
+          <dd class="about-info__desc">
+            建築物の設計及び監理、<br>リフォーム、リノベーション工事
+          </dd>
+        </div>
+
+        <div class="about-info__group js-fade-up">
+          <dt class="about-info__term">建築士事務所登録</dt>
+          <dd class="about-info__desc">
+            一級建築士事務所　登録番号 7806号
+          </dd>
+        </div>
+
+        <div class="about-info__group js-fade-up">
+          <dt class="about-info__term">一般建設業許可</dt>
+          <dd class="about-info__desc">
+            静岡県知事許可（般－06）第 41840 号
+          </dd>
+        </div>
+
+        <div class="about-info__group js-fade-up">
+          <dt class="about-info__term">加入</dt>
+          <dd class="about-info__desc">
+            建築士賠償責任補償制度（けんばい）
+          </dd>
+        </div>
+      </dl>
+    </div>
+  </section>
+
+  <section class="about-gallery">
+    <div class="about-gallery__inner">
+
+      <ul class="about-gallery__group about-gallery__group--top">
+        <li class="about-gallery__item about-gallery__item--01 js-scaleImg">
+          <img src="<?php echo IMAGEPATH; ?>/about/about-gallery-01.webp" alt="木天井と大開口が印象的なリビングダイニング" width="406" height="541" loading="lazy" class="about-gallery__img" />
+        </li>
+        <li class="about-gallery__item about-gallery__item--02 js-scaleImg">
+          <img src="<?php echo IMAGEPATH; ?>/about/about-gallery-02.webp" alt="切妻の大窓をもつ白い外観の住宅" width="338" height="451" loading="lazy" class="about-gallery__img" />
+        </li>
+      </ul>
+
+      <ul class="about-gallery__group about-gallery__group--bottom">
+        <li class="about-gallery__item about-gallery__item--03 js-scaleImg">
+          <img src="<?php echo IMAGEPATH; ?>/about/about-gallery-03.webp" alt="夕景に照らされた切妻窓の住宅外観" width="469" height="351" loading="lazy" class="about-gallery__img" />
+        </li>
+        <li class="about-gallery__item about-gallery__item--04 js-scaleImg">
+          <img src="<?php echo IMAGEPATH; ?>/about/about-gallery-04.webp" alt="白壁の通路と奥に見える作業テーブル" width="375" height="499" loading="lazy" class="about-gallery__img --delay-1" />
+        </li>
+      </ul>
 
     </div>
   </section>
+
+  <section class="about-access">
+    <div class="about-access__inner">
+
+      <h2 class="about-access__title section-title">
+        <img src="<?php echo IMAGEPATH; ?>/common/access-title.webp" alt="アクセス" width="132" height="29" loading="lazy" class="section-title__img">
+      </h2>
+
+      <ul class="about-access__list">
+        <li class="about-access__item about-access__item--01 js-fade-up">
+          <div class="about-access__content">
+            <div class="about-access__map">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6560.058498228677!2d137.7201371!3d34.7044422!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601adf104cac8b79%3A0x679b3069255b7dd1!2zb3Jv6Kit6KiI77yP5LiA57Sa5bu656-J5aOr5LqL5YuZ5omA77yI6bSo5rGf5LqL5YuZ5omA77yJ!5e0!3m2!1sja!2sjp!4v1762323794217!5m2!1sja!2sjp" width="418" height="332" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="about-access__info">
+              <p class="about-access__office-name">鴨江分室</p>
+              <p class="about-access__info-text">
+                住所：浜松市中央区鴨江1丁目7-19<br>
+                TEL：<a href="tel:053-570-8742">053-570-8742</a><br>
+                （9:30～18:30）<br>
+                駐車場：有<br>
+                ※場所等、下記参照ください。<br>
+                最寄駅浜松駅より徒歩15分<br>
+                遠鉄バス中電前より徒歩2分
+              </p>
+            </div>
+          </div>
+
+          <p class="about-access__note">
+            駐車場は事務所の裏手に用意があります。お手数ですが、車をお停めになって事務所までお越しになってください。駐車場は、細田商店さんの斜め向かいの細長い駐車場になります。お帰りの際、出庫が楽になりますのでバックでの駐車をお勧め致します。
+          </p>
+
+          <div class="about-access__schematic-wrap">
+            <img src="<?php echo IMAGEPATH; ?>/about/about-access-schematic.webp" alt="鴨江分室の周辺案内図イラスト" width="880" height="520" loading="lazy" class="about-access__schematic-img" />
+          </div>
+        </li>
+
+        <li class="about-access__item about-access__item--02 js-fade-up">
+          <div class="about-access__content">
+            <div class="about-access__map">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13099.932945686878!2d137.798505!3d34.831514!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601b1fac1c3fe359%3A0xaecc433da05f2a86!2zb3Jv6Kit6KiI77yP5LiA57Sa5bu656-J5aOr5LqL5YuZ5omA77yI5rWc5YyX5LqL5YuZ5omA77yJ!5e0!3m2!1sja!2sjp!4v1762325006731!5m2!1sja!2sjp" width="418" height="247" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="about-access__info">
+              <p class="about-access__office-name">於呂事務所</p>
+              <p class="about-access__info-text">
+                住所：浜松市浜名区於呂1475-1<br>
+                駐車場：有（敷地内1台）<br>
+                最寄駅：遠鉄浜北駅より徒歩20分
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+
+    </div>
+  </section>
+
+  <div class="about-top-btn">
+    <a href="<?php echo HOME_URL; ?>" class="link-btn-sub link-btn-sub--white">
+      <span class="link-btn-sub__text">トップに戻る</span>
+    </a>
+  </div>
 
 </main>
 
